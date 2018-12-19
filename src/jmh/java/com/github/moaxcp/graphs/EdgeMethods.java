a746503e8a2ea933ef0671c3d00aaceed4dbae27
John Mercier <moaxcp@gmail.com>
2018-12-18 23:34:58 -0500

    Updating EdgeMethods to support params.

--- src/jmh/java/com/github/moaxcp/graphs/EdgeMethods.java
+++ src/jmh/java/com/github/moaxcp/graphs/EdgeMethods.java
@@ -1,106 +1,68 @@
 package com.github.moaxcp.graphs;
 
 import com.github.moaxcp.graphs.SimpleGraph.Edge;
-import com.google.common.collect.*;
-import java.util.*;
+import java.util.Set;
 import org.openjdk.jmh.annotations.*;
 
+@State(Scope.Benchmark)
 public class EdgeMethods {
 
-    public static final int VERTICES = 200;
+    @Param({"2", "3", "4", "5", "6", "7", "8"})
+    public int vertices;
 
-    @State(Scope.Benchmark)
-    public static class UndirectedState {
-        SimpleGraph<Integer> graph;
+    @Param({"true", "false"})
+    public boolean directed;
 
-        @Setup(Level.Trial)
-        public void setup() {
-            graph = new UndirectedGraph<>();
-            ContiguousSet<Integer> integers = ContiguousSet.closedOpen(0, VERTICES);
-            Set<Set<Integer>> combinations = Sets.combinations(integers, 2);
-            for(Set<Integer> combination : combinations) {
-                Iterator<Integer> iterator = combination.iterator();
-                graph.edge(iterator.next(), iterator.next());
-            }
-            System.out.println("undirected graph");
-            System.out.println("    vertices: " + graph.getVertices().size());
-            System.out.println("    edges: " + graph.getEdges().size());
-            assert graph.getVertices().size() == VERTICES;
-            assert graph.findEdge(VERTICES - 2, VERTICES - 1).isPresent();
-        }
-    }
+    private SimpleGraph<Integer> graph;
+    private Integer lastFrom;
+    private Integer lastTo;
 
-    @State(Scope.Benchmark)
-    public static class DirectedState {
-        SimpleGraph<Integer> graph;
-
-        @Setup(Level.Trial)
-        public void setup() {
+    @Setup(Level.Trial)
+    public void setup() {
+        if(directed) {
             graph = new DirectedGraph<>();
-            ContiguousSet<Integer> integers = ContiguousSet.closedOpen(0, VERTICES);
-            Set<Set<Integer>> combinations = Sets.combinations(integers, 2);
-            for(Set<Integer> combination : combinations) {
-                Iterator<Integer> iterator = combination.iterator();
-                Integer first = iterator.next();
-                Integer second = iterator.next();
-                graph.edge(first, second);
-                graph.edge(second, first);
+            for(int i = 0; i < vertices; i++) {
+                for(int j = 0; j < vertices; j++) {
+                    graph.edge(i, j);
+                    graph.edge(j, i);
+                    lastFrom = j;
+                    lastTo = i;
+                }
             }
-            System.out.println("directed graph");
-            System.out.println("    vertices: " + graph.getVertices().size());
-            System.out.println("    edges: " + graph.getEdges().size());
-            assert graph.getVertices().size() == VERTICES;
-            assert graph.findEdge(VERTICES - 2, VERTICES - 1).isPresent();
+        } else {
+            graph = new UndirectedGraph<>();
+            for(int i = 0; i < vertices; i++) {
+                for(int j = 0; j < vertices; j++) {
+                    graph.edge(i, j);
+                    lastFrom = i;
+                    lastTo = j;
+                }
+            }
         }
     }
 
     @Benchmark
-    public Edge<Integer> undirectedFindLastEdge(UndirectedState undirectedState) {
-        return undirectedState.graph.edge(VERTICES - 2, VERTICES - 1);
+    public Edge<Integer> findLastEdge() {
+        return graph.edge(lastFrom, lastTo);
     }
 
     @Benchmark
-    public Edge<Integer> undirectedFindFirstEdge(UndirectedState undirectedState) {
-        return undirectedState.graph.edge(0, 0);
+    public Edge<Integer> findFirstEdge() {
+        return graph.edge(0, 0);
     }
 
     @Benchmark
-    public Set<Edge<Integer>> undirectedAdjaentEdges(UndirectedState undirectedState) {
-        return undirectedState.graph.vertex(0).adjacentEdges();
+    public Set<Edge<Integer>> adjaentEdges() {
+        return graph.vertex(lastTo).adjacentEdges();
     }
 
     @Benchmark
-    public Set<Edge<Integer>> undirectedInEdges(UndirectedState undirectedState) {
-        return undirectedState.graph.vertex(0).inEdges();
+    public Set<Edge<Integer>> inEdges() {
+        return graph.vertex(lastTo).inEdges();
     }
 
     @Benchmark
-    public Set<Edge<Integer>> undirectedOutEdges(UndirectedState undirectedState) {
-        return undirectedState.graph.vertex(0).outEdges();
-    }
-
-    @Benchmark
-    public Edge<Integer> directedFindLastEdge(DirectedState directedState) {
-        return directedState.graph.edge(VERTICES - 2, VERTICES - 1);
-    }
-
-    @Benchmark
-    public Edge<Integer> directedFindFirstEdge(DirectedState directedState) {
-        return directedState.graph.edge(0, 0);
-    }
-
-    @Benchmark
-    public Set<Edge<Integer>> directedAdjaentEdges(DirectedState directedState) {
-        return directedState.graph.vertex(0).adjacentEdges();
-    }
-
-    @Benchmark
-    public Set<Edge<Integer>> directedInEdges(DirectedState directedState) {
-        return directedState.graph.vertex(0).inEdges();
-    }
-
-    @Benchmark
-    public Set<Edge<Integer>> directedOutEdges(DirectedState directedState) {
-        return directedState.graph.vertex(0).outEdges();
+    public Set<Edge<Integer>> outEdges() {
+        return graph.vertex(lastTo).outEdges();
     }
 }
