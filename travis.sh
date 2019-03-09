92924eb92d09c7551453fdfc19f65f038c50cfed
John Mercier <moaxcp@gmail.com>
2019-03-09 15:54:29 -0500

    Working on automatic release to nexus

--- travis.sh
+++ travis.sh
@@ -6,17 +6,21 @@ if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
     exit 1
 fi
 
-publish() {
+scan() {
     # update sonar (including failing tests)
     ./gradlew -x test build
     ./gradlew test || true
     git fetch --unshallow #get all commit history for exact blame info
     ./gradlew -x test -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=moaxcp -Dsonar.login=$SONAR_TOKEN sonarqube
+}
 
+build() {
     # run build
     ./gradlew build
+}
 
-    # publish snapshot
+publish() {
+    # publish
     ./gradlew publish \
         -Dnexus.username=moaxcp \
         -Dnexus.password=$NEXUS_PASSWORD \
@@ -27,15 +31,17 @@ publish() {
 
 if [ -n "$TRAVIS_TAG" ]; then
     echo "release for $TRAVIS_TAG"
+    build
     publish
 
     ./gradlew closeAndReleaseRepository --info --stacktrace \
         -Dnexus.username=moaxcp \
         -Dnexus.password=$NEXUS_PASSWORD
 
-elif [ "$TRAVIS_BRANCH" == "master" ]; then
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "build for master branch"
-    publish
+    scan
+    build
 else
     echo "build for different branch not yet supported"
     exit 1
