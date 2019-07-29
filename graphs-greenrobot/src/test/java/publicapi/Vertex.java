e66a42eb0fbfc94257f2aca411c46709a2883b25
John Mercier <moaxcp@gmail.com>
2019-07-28 20:42:50 -0400

    Adding tests for vertex properties events.

--- graphs-greenrobot/src/test/java/publicapi/Vertex.java
+++ graphs-greenrobot/src/test/java/publicapi/Vertex.java
@@ -45,10 +45,94 @@ public class Vertex {
     }
 
     @EventSimpleGraphs
-    void addProperty(EventGraph<String> graph, EventBus bus) {
+    void addProperty1(EventGraph<String> graph, EventBus bus) {
         graph.id("graph");
         graph.vertex("A");
         assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name", "value")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty2(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty3(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty4(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty5(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty6(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5", "name6", "value6")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty7(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5", "name6", "value6", "name7", "value7")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty8(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5", "name6", "value6", "name7", "value7", "name8", "value8")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty9(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5", "name6", "value6", "name7", "value7", "name8", "value8", "name9", "value9")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void addProperty10(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.vertex("A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3", "name4", "value4", "name5", "value5", "name6", "value6", "name7", "value7", "name8", "value8", "name9", "value9", "name10", "value10")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void updateProperty1(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.getVertex("A").property("name", "A");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name", "value1")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void updateProperty2(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.getVertex("A").property("name1", "A", "name2", "B");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2")).containsExactly(VertexPropertiesEvent.class);
+    }
+
+    @EventSimpleGraphs
+    void updateProperty3(EventGraph<String> graph, EventBus bus) {
+        graph.id("graph");
+        graph.getVertex("A").property("name1", "A", "name2", "B", "name3", "C");
+        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name1", "value1", "name2", "value2", "name3", "value3")).containsExactly(VertexPropertiesEvent.class);
     }
 
     @EventSimpleGraphs
@@ -56,12 +140,5 @@ public class Vertex {
         graph.id("graph");
         graph.getVertex("A").property("name", "value");
         assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").removeProperty("name")).containsExactly(VertexPropertyRemoved.class);
-    }
-
-    @EventSimpleGraphs
-    void updateProperty(EventGraph<String> graph, EventBus bus) {
-        graph.id("graph");
-        graph.getVertex("A").property("name", "value");
-        assertThat(graph).with(bus).hasEventsIn(g-> g.getVertex("A").property("name", "value2")).containsExactly(VertexPropertiesEvent.class);
     }
 }
