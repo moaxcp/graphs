3ad52869b9903068566a4b72000aee391ad578b0
John Mercier <moaxcp@gmail.com>
2019-11-09 13:08:29 -0500

    Adding cacert package and setting variable

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --show-trace --pure --keep encrypted_de4911fc7d4a_key --keep encrypted_de4911fc7d4a_iv --keep TRAVIS_PULL_REQUEST --keep TRAVIS_TAG --keep TRAVIS_BRANCH --keep GIT_SSL_CAINFO --keep SSL_CERT_FILE -i bash -p git graphviz adoptopenjdk-bin openssl
+#! nix-shell --show-trace --pure --keep encrypted_de4911fc7d4a_key --keep encrypted_de4911fc7d4a_iv --keep TRAVIS_PULL_REQUEST --keep TRAVIS_TAG --keep TRAVIS_BRANCH -i bash -p git cacert graphviz adoptopenjdk-bin openssl
 
 set -euo pipefail
 
@@ -36,6 +36,7 @@ neato -V
 openssl version
 
 JAVA_HOME=$(readlink -e $(type -p javac) | sed  -e 's/\/bin\/javac//g')
+GIT_SSL_CAINFO=~/.nix-profile/etc/ssl/certs/ca-bundle.crt
 
 echo "JAVA_HOME is ${JAVA_HOME}"
 
