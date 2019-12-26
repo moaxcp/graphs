23f6d5836803a16fd8b4f922f3ed16265fedc144
John Mercier <moaxcp@gmail.com>
2019-12-26 15:52:34 -0500

    Fixing build issue

--- graphs-core/src/main/java/com/github/moaxcp/graphs/EventGraph.java
+++ graphs-core/src/main/java/com/github/moaxcp/graphs/EventGraph.java
@@ -1,8 +1,7 @@
 package com.github.moaxcp.graphs;
 
 /**
- * An implementation of graphs which sends {@link com.github.moaxcp.graphs.events.GraphEvent}s when the graph is
- * modified.
+ * An implementation of graphs which sends events when the graph is modified.
  * @param <ID> type of all identifiers in graph
  */
 public interface EventGraph<ID> extends Graph<ID> {
