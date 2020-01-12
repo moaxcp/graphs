5a609d7afb180bf011e0ec531c5e61367d1c0c88
John Mercier <moaxcp@gmail.com>
2020-01-11 23:22:17 -0500

    Adding test for test framework so jacoco report is generated.

---
+++ graphs-testframework/src/test/java/com/github/moaxcp/graphs/testframework/MethodSourcesTest.java
@@ -0,0 +1,16 @@
+package com.github.moaxcp.graphs.testframework;
+
+import com.github.moaxcp.graphs.*;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static com.github.moaxcp.graphs.testframework.MethodSources.*;
+import static java.util.stream.Collectors.*;
+
+public class MethodSourcesTest {
+  @Test
+  void testSimpleGraphs() {
+    List<Graph<String>> result = simpleGraphs().collect(toList());
+  }
+}
